331aeff39cc3c3143873c2fde16f88ba7932fa49
Mohit Sharma <mohit.prasadsharma@dubizzle.com>
2019-01-13 17:05:33 +0400

    WIP : Android test for splash screen

--- app/src/test/java/com/codelife/dubizzle/ExampleUnitTest.kt
+++ app/src/test/java/com/codelife/dubizzle/ExampleUnitTest.kt
@@ -1,17 +1,34 @@
 package com.codelife.dubizzle
 
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.intent.Intents.getIntents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.ComponentNameMatchers
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.ext.truth.content.IntentSubject.assertThat
+import com.codelife.dubizzle.ui.home.HomeActivity
+import com.codelife.dubizzle.ui.home.SplashActivity
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun splashActivityTest() {
+
+        val scenario = ActivityScenario.launch(SplashActivity::class.java)
+        assertThat(getIntents().first()).hasComponentClass(HomeActivity::class.java)
+
+
     }
 }
