370181822c180e82e85906d606f20e703916cb74
Mohit Sharma <mohit.prasadsharma@dubizzle.com>
2019-01-17 10:47:09 +0400

    unit test for splash activity

--- app/src/test/java/com/codelife/dubizzle/ExampleUnitTest.kt
+++ app/src/test/java/com/codelife/dubizzle/ExampleUnitTest.kt
@@ -1,15 +1,13 @@
 package com.codelife.dubizzle
 
-import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.intent.Intents.getIntents
 import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.ComponentNameMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.ext.truth.content.IntentSubject.assertThat
 import com.codelife.dubizzle.ui.home.HomeActivity
 import com.codelife.dubizzle.ui.home.SplashActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -23,12 +21,13 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
 
+    @get:Rule
+    val rule = IntentsTestRule(SplashActivity::class.java)
+
     @Test
     fun splashActivityTest() {
-
-        val scenario = ActivityScenario.launch(SplashActivity::class.java)
-        assertThat(getIntents().first()).hasComponentClass(HomeActivity::class.java)
-
-
+        ActivityScenario.launch(SplashActivity::class.java)
+        //assertThat(getIntents().first()).hasComponentClass(HomeActivity::class.java)
+        intended(IntentMatchers.hasComponent(HomeActivity::class.java.canonicalName))
     }
 }
